b915feef2cd92d411d5ab0e9b89a9a073da669ec
Branislav Bajlovski <branislav@sourcico.com>
2024-12-02 14:38:05 +0100

    Improved App starter

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import { createInterface } from "readline/promises";
 import { DayView } from "./DayView";
 
 
-
-
 const main = async () => {
     const readline = createInterface({
         input: process.stdin,
@@ -25,33 +23,3 @@ const main = async () => {
 }
 
 main();
-
-// const question = (questionText: string) =>
-//     new Promise<string>(resolve => readline.question(questionText, resolve))
-//         .finally(() => readline.close());
-
-// console.clear();
-// console.log("##################################");
-// console.log("# Welcome to Advent of Code 2024 #");
-// console.log("##################################\n");
-
-// const answer = async (prompt: string) => {
-//     // const prompt = await question("Pick a Day: ")
-//     let dayView: DayView = new DayView(prompt);
-//     await dayView.resolve();
-//     console.log(`\n--> Day ${dayView.id} (data: ${dayView.input})`);
-//     console.log(`----> Part 1: ${dayView.answerOne} (${dayView.executionTimeOne}ms)`);
-//     console.log(`----> Part 2: ${dayView.answerTwo} (${dayView.executionTimeTwo}ms)`);
-// };
-
-
-
-// // Loop question until answer from console is correct
-// readline.on("close", async () => {
-//     const prompt = await question("Pick a Day (or type Q/q to quit): ");
-//     if (prompt.toLowerCase() === "q") {
-//         process.exit(0);
-//     } else {
-//         await answer(prompt);
-//     }
-// });
